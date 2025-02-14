aaee6aa274fe376a5e4df9445463729c9e7a0558
yasmeena <yasog766@gmail.com>
2025-02-14 20:10:34 +0200

    yasmeen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gym_app/setup_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,17 @@ class MainApp extends StatelessWidget {
 ////////////////////////////////////////////////////
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MotivationScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      minTextAdapt: true,
+      splitScreenMode: true,
+       builder: (_, child){
+        return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MotivationScreen(),
+    );
+    }
+
     );
   }
 }
