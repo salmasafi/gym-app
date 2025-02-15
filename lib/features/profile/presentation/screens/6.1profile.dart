54037595891372cede9d8734abf90b10e5877ab9
yasmeena <yasog766@gmail.com>
2025-02-16 00:09:28 +0200

    profile6.1

--- lib/features/profile/presentation/screens/6.1profile.dart
+++ lib/features/profile/presentation/screens/6.1profile.dart
@@ -9,15 +9,9 @@ class SixdotOneProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*appBar: AppBar(
-        title: Text("My Profile"),
-      ),*/
+      backgroundColor: Colors.black87,
       body:Body(),
     );
-    /*return const ScreenUtilInit(
-      designSize: const Size(393,852),
-      minTextAdapt: true,
-      splitScreenMode: true,
-    );*/
+
   }
 }
--- lib/features/profile/presentation/widgets/profilebody.dart
+++ lib/features/profile/presentation/widgets/profilebody.dart
@@ -7,41 +7,64 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
+        SizedBox(
           width:393 ,
-          height: 289,
-          color: Color(0xffB3A0FF),
-          child: Column(
+          height: 320,
+          child: Stack(
             children: [
-              SizedBox( height: 30),
-              Row(
-                children: [
-                  Icon(Icons.arrow_left),
-
-                  Text("My Profile",style: TextStyle(
-                    fontSize: 20,fontWeight: FontWeight.w700,color: Colors.white
-                  ),),
-                ],
-              ),
-              Positioned(
-                top: 95,
-                left: 132,
-                child: SizedBox(
-                  width: 125,
-                  height: 125,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/profile.png"),
-                  ),
+              Container(
+                width:393 ,
+                height: 289,
+                color: Color(0xffB3A0FF),
+                child: Column(
+                  children: [
+                    SizedBox(height: 30,),
+                    Row(
+                      children: [
+                        Icon(Icons.arrow_left),
+                        Text("My Profile",style: TextStyle(
+                          fontSize: 20,fontWeight: FontWeight.w700,color: Colors.white
+                        ),),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 125,
+                      height: 125,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage("assets/images/profile.png"),
+                      ),
+                    ),
+                    Text("name"),
+                    Text("email.com"),
+                    Text("Birthday:date"),
+                  ],
                 ),
               ),
-              Text("name"),
-              Text("email.com"),
-              Text("Birthday:date"),
-
+              Positioned(
+                  top: 260,
+                  left: 36,
+                  child: Container(
+                    width: 323,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xff896CFE),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text("data"),
+                            Text("data"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+              ),
             ],
           ),
         ),
-
       ],
     );
   }
