c187661ade93cb6f25330608508477b941d17454
yasmeena <yasog766@gmail.com>
2025-02-16 22:50:38 +0200

    edit profile

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:gym_app/features/profile/presentation/screens/6.1profile.dart';
 
 //import 'package:gym_app/setup_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gym_app/features/set_up/presentation/screens/setup_screen.dart';
 import 'package:gym_app/setup_screen.dart';
 
 void main() {
@@ -23,9 +24,9 @@ class MainApp extends StatelessWidget {
        builder: (_, child){
         return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        //home: MotivationScreen(),
+        home: MotivationScreen(),
          // home: SixdotOneProfileScreen(),
-          home: ProfileEdit(),
+          //home: ProfileEdit(),
     );
     }
 
